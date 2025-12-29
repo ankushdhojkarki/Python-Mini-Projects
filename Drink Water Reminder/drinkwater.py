50bd528d58d40b68b5d639270ebc57c9ebbd9f28
Ankush Karki <ankushdhojkarki@gmail.com>
2025-12-29 16:21:27 +0545

    Fix for drink water reminder and files setup for dice rolling game

--- Drink Water Reminder/drinkwater.py
+++ Drink Water Reminder/drinkwater.py
@@ -5,4 +5,4 @@ while True:
     print("Please sip some water!")
     notification.notify(title="Reminder to drink water",
                         message="Please drink some water to stay hydrated.")
-    time.sleep(60*60)  # 1 hour reminder
+    time.sleep(3)  # 1 hour reminder
